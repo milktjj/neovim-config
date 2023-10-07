67ec4c648e10c48cc56c88151fcc62f58f6025fc
milk <milktjj@outlook.com>
2023-10-07 14:29:19 +0800

    add keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -12,6 +12,7 @@ keymap.set("i", "jk", "<ESC>")
 vim.keymap.set('n', '<C-s>', ':w<CR>')          -- Save
 vim.keymap.set('v', '<C-c>', '"+y')             -- Copy
 vim.keymap.set('n', '<C-v>', '"+P')             -- Paste normal mode
+vim.keymap.set('t', '<C-v>', '"+P')             -- Paste normal mode
 vim.keymap.set('v', '<C-v>', '"+P')             -- Paste visual mode
 vim.keymap.set('c', '<C-v>', '<C-R>+')          -- Paste command mode
 vim.keymap.set('i', '<C-v>', '<ESC>l"+Pli')     -- Paste insert mode
